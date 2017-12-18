10f26a28464ecfc2a5eb30d0fe307382eee2670c
brightmoon <892206397@qq.com>
2017-12-18 18:57:27 +0800

    ..

--- src/page/book/book.vue
+++ src/page/book/book.vue
@@ -1,12 +1,12 @@
 <template lang="html">
   <div class="box">
-    <div class="toptab">
+    <div class="toptab" v-if="showSet">
       <span><返回书架</span>
     </div>
-    <div class="content" @touchstart="setStyle" :class="classList[cindex]">
+    <div class="content" ref="content" @touchstart="setStyle" :class="className">
       {{content}}
     </div>
-    <div class="footertab">
+    <div class="footertab" v-if="showSet">
       <div class="page">
         <span>上一章</span>
         <span>下一章</span>
@@ -14,8 +14,17 @@
       <div class="set">
         <span>目录</span>
         <span>关灯</span>
-        <span>选项</span>
+        <span @touchstart="option">选项</span>
         <span>更多...</span>
+      </div>
+    </div>
+    <div class="option-set" v-if="showOption">
+      <div class="big-small">
+        <span class="big" @touchstart="fontChange('big')">大</span>
+        <span class="small" @touchstart="fontChange('small')">小</span>
+      </div>
+      <div class="round-div">
+        <span class="round" v-for="item in classList" :class="item" @touchstart="bgstyle(item)"></span>
       </div>
     </div>
   </div>
@@ -26,8 +35,10 @@ export default {
   data() {
     return {
       content: '',
-      classList: ['bgcw', 'bgcg', 'bgcye'],
-      cindex: 0
+      showSet: false,
+      showOption: false,
+      className: '',
+      classList: ['bgcw', 'bgcg', 'bgcye', 'bgcred', 'bgcmo']
     }
   },
   created() {
@@ -40,10 +51,22 @@ export default {
     },
     setStyle() {
       console.log('setStyle');
-      if (this.cindex < 2) {
-        this.cindex++;
-      } else {
-        this.cindex = 0;
+      this.showSet = true;
+    },
+    bgstyle(res) {
+      this.className = res;
+    },
+    option() {
+      this.showSet = false;
+      this.showOption = true;
+    },
+    fontChange(res){
+      switch (res) {
+        case 'big':
+
+          break;
+        default:
+
       }
     }
   }
@@ -65,20 +88,56 @@ export default {
         line-height: 2em;
     }
 }
-.footertab{
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  height: 80px;
-  background-color: #ccc;
-  .page{
-    display: flex;
-    justify-content: space-around;
-  }
-  .set{
-    display: flex;
-    justify-content: space-around;
-  }
+.footertab {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 80px;
+    background-color: #ccc;
+    .page {
+        display: flex;
+        justify-content: space-around;
+        span {
+            line-height: 2em;
+        }
+    }
+    .set {
+        display: flex;
+        justify-content: space-around;
+        span {
+            line-height: 2em;
+        }
+    }
+}
+.option-set {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 80px;
+    background-color: #ccc;
+    padding: 5px 0;
+    .big-small {
+        display: flex;
+        justify-content: space-around;
+        margin: 5px;
+        span {
+            display: inline-block;
+            width: 40%;
+            text-align: center;
+            border: 1px solid #000;
+            border-radius: 5px;
+        }
+    }
+    .round-div {
+        display: flex;
+        justify-content: space-around;
+        .round {
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            border-radius: 10px;
+        }
+    }
 }
 .bgcw {
     background-color: #fff;
@@ -88,5 +147,11 @@ export default {
 }
 .bgcye {
     background-color: yellow;
+}
+.bgcred {
+    background-color: red;
+}
+.bgcmo {
+    background-color: #eac;
 }
 </style>
